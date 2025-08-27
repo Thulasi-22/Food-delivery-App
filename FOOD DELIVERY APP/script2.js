c76de3efacc9ea2ad89471879255ec9f183a39ae
thulaseswarareddy Bannuru <23am1a31d1@svrec.ac.in>
2025-08-27 19:18:20 +0530

    Add files via upload

--- FOOD DELIVERY APP/script2.js
+++ FOOD DELIVERY APP/script2.js
@@ -1,4 +1,4 @@
-console.log("hello dvbfh")
+console.log("hello dvbfh fegs")
 let order1=document.getElementById('order1')
 let order2=document.getElementById('order2')
 let order3=document.getElementById('order3')
@@ -8,35 +8,75 @@ let order6=document.getElementById('order6')
 let order7=document.getElementById('order7')
 let order8=document.getElementById('order8')
 let order9=document.getElementById('order9')
-let cancel=document.getElementById('cancel')
+let cancel1=document.getElementById('cancel1')
+let cancel2=document.getElementById('cancel2')
+let cancel3=document.getElementById('cancel3')
+let cancel4=document.getElementById('cancel4')
+let cancel5=document.getElementById('cancel5')
+let cancel6=document.getElementById('cancel6')
+let cancel7=document.getElementById('cancel7')
+let cancel8=document.getElementById('cancel8')
+let cancel9=document.getElementById('cancel9')
 order1.addEventListener('click',()=>{
     order1.textContent='Confirm';
-    document.getElementById('cancel').style.display='inline-block';
+    document.getElementById('cancel1').style.display='inline-block';
 })
 order2.addEventListener('click',()=>{
     order2.textContent='Confirm';
+    document.getElementById('cancel2').style.display='inline-block';
 })
 order3.addEventListener('click',()=>{
     order3.textContent='Confirm';
+    document.getElementById('cancel3').style.display='inline-block';
 })
 order4.addEventListener('click',()=>{
     order4.textContent='Confirm';
+    document.getElementById('cancel4').style.display='inline-block';
 })
 order5.addEventListener('click',()=>{
     order5.textContent='Confirm';
+    document.getElementById('cancel5').style.display='inline-block';
 })
 order6.addEventListener('click',()=>{
     order6.textContent='Confirm';
+    document.getElementById('cancel6').style.display='inline-block';
 })
 order7.addEventListener('click',()=>{
     order7.textContent='Confirm';
+    document.getElementById('cancel7').style.display='inline-block';
 })
 order8.addEventListener('click',()=>{
     order8.textContent='Confirm';
+    document.getElementById('cancel8').style.display='inline-block';
 })
 order9.addEventListener('click',()=>{
     order9.textContent='Confirm';
+    document.getElementById('cancel9').style.display='inline-block';
 })
-cancel.addEventListener('click',()=>{
-    cancel.textContent='Cancelled'
+cancel1.addEventListener('click',()=>{
+    cancel1.textContent='Cancelled'
+})
+cancel2.addEventListener('click',()=>{
+    cancel2.textContent='Cancelled'
+})
+cancel3.addEventListener('click',()=>{
+    cancel3.textContent='Cancelled'
+})
+cancel4.addEventListener('click',()=>{
+    cancel4.textContent='Cancelled'
+})
+cancel5.addEventListener('click',()=>{
+    cancel5.textContent='Cancelled'
+})
+cancel6.addEventListener('click',()=>{
+    cancel6.textContent='Cancelled'
+})
+cancel7.addEventListener('click',()=>{
+    cancel7.textContent='Cancelled'
+})
+cancel8.addEventListener('click',()=>{
+    cancel8.textContent='Cancelled'
+})
+cancel9.addEventListener('click',()=>{
+    cancel9.textContent='Cancelled'
 })
